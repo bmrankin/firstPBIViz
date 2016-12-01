bb127974bcf5166b067331ba13591d83b95b0668
Bryan Rankin <bryan@bryanrankin.com>
2016-12-01 09:22:04 -0500

    Initial Commit

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var PBI_CV_301E1EE3_EB84_4343_98EE_88DC3FC04D2F_DEBUG = {
+        name: 'PBI_CV_301E1EE3_EB84_4343_98EE_88DC3FC04D2F_DEBUG',
+        displayName: 'First PBI Viz',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.3.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.PBI_CV_301E1EE3_EB84_4343_98EE_88DC3FC04D2F.Visual(options),
+        custom: true
+    };
+}
